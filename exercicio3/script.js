dc4dcbaa2f05501359568384d6037ec7c5a2a67e
GuilhermeH6 <guilhermeholanda870@gmail.com>
2022-08-23 15:44:07 -0300

    corrigindo o exercicio 3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,10 @@
 // # Exercício 3
 
-// Peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário sendo o argumento. Por fim, imprima no console o resultado das operações
-let num1 = +prompt('insira o primeiro numero')
-let num2 = +prompt('insira o segundo numero')
-
 // Crie 4 funções, cada uma deve receber dois parâmetros numéricos e retornar o valor uma das operações básicas (soma, subtração, multiplicação e divisão).
 function soma(num1, num2) {
     let resultado = num1 + num2
     console.log(resultado)
 }
-
-soma(num1, num2)
 
 ///////////////////////////////////////////////
 
@@ -19,16 +13,12 @@ function subtrair(num1, num2) {
     console.log(resultado)
 }
 
-subtrair(num1, num2)
-
 //////////////////////////////////////////////
 
 function multiplicar(num1, num2) {
     let resultado = num1 * num2
     console.log(resultado)
 }
-
-multiplicar(num1, num2)
 
 ///////////////////////////////////////////////
 
@@ -37,5 +27,12 @@ function dividir(num1, num2) {
     console.log(resultado)
 }
 
+// Em seguida, peça para o usuário inserir dois números e chame essas 4 funções com esses valores inputados pelo usuário sendo o argumento. Por fim, imprima no console o resultado das operações
+let num1 = +prompt('insira o primeiro numero')
+let num2 = +prompt('insira o segundo numero')
+
+soma(num1, num2)
+subtrair(num1, num2)
+multiplicar(num1, num2)
 dividir(num1, num2)
 
